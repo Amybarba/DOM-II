b8653ec3bf60e9f5fd1b08e286083e71ae2460b9
Amy Barba <Amybarba70@gmail.com>
2019-06-24 00:47:15 -0700

    recheck project for missing checkmarks

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 * [X] Add your project manager as collaborator on Github.
 * [X] Clone your OWN version of the repository (Not Lambda's by mistake!).
 * [X] Create a new branch: git checkout -b `<firstName-lastName>`.
-* [ ] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-* [ ] Push commits: git push origin `<firstName-lastName>`.
+* [X] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
+* [X] Push commits: git push origin `<firstName-lastName>`.
 
 **Follow these steps for completing your project.**
 
@@ -33,22 +33,26 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 ## Task 2: Create Unique Event Listeners
 
-* [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
-	* [ ] `keydown`
-	* [ ] `wheel`
-	* [ ] `drag / drop`
-	* [ ] `load`
+* [X] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
+	* [X] `mouseover`
+	* [X] `keypress`
+	* [X] `wheel`
+	* [X] `drag / drop`
+	* [X] `load`
 	* [ ] `focus`
 	* [ ] `resize`
 	* [ ] `scroll`
 	* [ ] `select`
-	* [ ] `dblclick`
+	* [X] `dblclick`
+	* [X] `alert`
+	* [X] `click`
+	* [X] `mouseenter`
+
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
-* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
-* [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+* [X] Nest two similar events somewhere in the site and prevent the event propagation properly
+* [X] Stop the navigation from items from refreshing the page by using `preventDefault()`
 
 ## Stretch Task:
 
--- js/index.js
+++ js/index.js
@@ -6,52 +6,36 @@ button.addEventListener('click', function() {
 window.scrollTo(0, 1000);
 console.log('WOW')
 });
-// 1  button to jump around sight upon click
-
-// failed code
-// const time = document.querySelector('container home');
-
-// time.addEventListener('click', function (event) {
-// time = event.timeStamp;
-// console.log(event.timeStamp);
-// });
-
-// W3 html solution
-// function myFunction(event) {
-//     var n = event.timeStamp;
-//     document.getElementById("demo").innerHTML = n;
+// 1  button to jump around sight upon click - Click
 
 const funBusHtPnk = document.querySelector('.intro');
 
 funBusHtPnk.addEventListener('mouseenter', function (event) {
     event.target.style.backgroundColor = 'hotpink';
 });
-// 2 Turn fun bus background Pink
+// 2 Turn fun bus background Pink - Mouseenter
 
 const conDest = document.querySelector('.content-destination');
 
 conDest.addEventListener('mouseenter', function (event) {
     event.target.style.backgroundColor = 'limegreen';
 }); 
-// 3 Turn boat picture background lime green
+// 3 Turn boat picture background lime green - Mouseenter
 
-// failed code
-// const logClick = document.querySelector('body');
+const logClick = document.querySelector('body');
 
-// logClick.addEventListener('keypress', function(event) {
-// console.log(event);
-// console.log('button clicked')
-//     // console.log(event.target.value);
-//     // const capturedInput = event.target.value;
-//     // button.textContent = capturedInput;
-// });
+window.addEventListener('keypress', function(event) {
+console.log(event);
+console.log('keys pressed')
+});
+// / 4 Log when keys are pressed on the page - Keypress
 
 const foot = document.querySelector('.footer');
 
 foot.addEventListener('mouseover', function (event) {
     event.target.style.backgroundColor = 'purple';
 }); 
-// 4 Turn footer background purple
+// 5 Turn footer background purple - Mouseover
 
 const funBusName = document.querySelector('.container.home');
 
@@ -59,11 +43,73 @@ funBusName.addEventListener('wheel', function (event) {
     event.stopPropagation();
     alert ( 'Yellow Submarine');
 });
-// 5 Name the bus
+// 6 Name the bus - Alert
+
+const signUp = document.querySelectorAll('.btn');
+
+signUp[0].addEventListener('click',  event => {
+    signUp[0].style.color = 'yellow';
+    signUp[0].style.backgroundColor = 'blue';
+    signUp[1].style.color = 'limegreen';
+    signUp[1].style.backgroundColor = 'hotpink';
+    signUp[2].style.color = 'lightblue';
+    signUp[2].style.backgroundColor = 'lightpink';
+}); 
+// 7 Turn button background blue, text yellow - Click/Nested - Thank You Tyree
+
+const funBusNameSwitch = document.querySelector('.logo-heading');
+
+funBusNameSwitch.addEventListener('dblclick', function (event) {
+funBusNameSwitch.textContent =
+  'Yellow Submarine';
+});
+// // 8 Name the bus with a - Dblclick
+
+const coffeeDrag = document.querySelector('.img-content');
+
+coffeeDrag.addEventListener('drag', function(event) {
+    event.stopPropagation();
+    console.log('Grab that coffee!!!');
+});
+// 9 Drag picture - Drag
+
+const loadPage = document.querySelector('body');
+
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+});
+// 10 message appears when - load
+
+const Adventure = document.querySelectorAll('.text-content');
+
+for (let i = 0; i < Adventure.length; i++) {
+    Adventure[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        alert(`Welcome`);
+    });
+}
+
+// 11 message appears using Click/preventDefault
 
 
 
+// Codes that failed:
 
+// languagechange
+// const lang = document.querySelector('.main-navigation');
 
+// window.addEventListener('languagechange', function() {
+//     console.log('languagechange event detected!'); 
+//   });
 
+// TimeStamp
+// const time = document.querySelector('.container .home');
 
+// time.addEventListener('click', function (event) {
+// window.timeStamp();
+// console.log(event.timeStamp);
+// });
+// W3 html solution
+// function myFunction(event) {
+//     var n = event.timeStamp;
+//     document.getElementById("demo").innerHTML = n;
